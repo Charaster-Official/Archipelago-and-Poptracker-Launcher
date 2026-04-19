f7f718c4e5f6bee65b646fd00a69b901571c02bc
Charaster <159323725+Charaster-Official@users.noreply.github.com>
2026-04-19 23:49:43 +0200

    bat file

---
+++ AP + POPTracker Launcher Public.bat
@@ -0,0 +1,19 @@
+@echo off
+title Archipelago and Poptracker Launcher
+
+:: you can remove this if you want, just a small warning thing
+echo Archipelago and Poptracker Launcher
+echo.
+echo Yo waddup, quick message that the Archipelago launcher and poptracker
+echo will also output a bit of debug messaging so to say
+echo this is normal don't worry.
+pause
+
+echo Opening Archipelago...
+:: path leading to your ArchipelagoLauncher.exe
+start "" "path to your ap launcher.exe here"
+echo Opening Poptracker
+:: path leading to your poptracker.exe
+start "" "path to your pop tracker.exe here"
+
+pause
